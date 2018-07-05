114b34ace894fdd7ca47b8d8690c8636e121ffeb
enderb <barillas.ender@gmail.com>
2018-07-04 22:04:16 -0500

    Implemented a search box, learned about state in React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots'
+import SearchBox from './SearchBox';
+import { robots } from './robots';
 
-const App = () => {
-    return (
-        <div>
-            <h1>Robo Friends</h1>
-            <CardList robots={robots}/>
-        </div>
-    )
+class App extends React.Component{
+    constructor() {
+        super()
+        this.state = {
+            robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter((robot) => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
+        return (
+            <div className='tc'>
+                <h1>Robo Friends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots}/>
+            </div>
+        )
+    }
+    
 }
 
 export default App;
